d21e8523425a0853d6b42a2797fc63e3af9c5e5e
fredyfloreslemus <fredyfloreslemus@gmail.com>
2025-03-24 12:50:30 -0600

    update terraform

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -12,8 +12,3 @@ output "cluster_name" {
   description = "Name of the EKS cluster"
   value       = module.eks.cluster_name
 }
-
-output "node_group_role_arn" {
-  description = "ARN of the EKS worker nodes IAM role"
-  value       = module.eks.node_groups["eks_nodes"].iam_role_arn
-}
