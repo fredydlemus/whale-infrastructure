f06da18effc64775f37f3d7ef01180942bacc8c0
fredyfloreslemus <fredyfloreslemus@gmail.com>
2025-03-24 12:52:27 -0600

    update terraform

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -27,7 +27,7 @@ module "vpc" {
 module "eks" {
   source       = "../../modules/eks"
   cluster_name = "dev-cluster"
-  eks_version  = "1.21"
+  eks_version  = "1.29"
   vpc_id       = module.vpc.vpc_id
   subnets      = module.vpc.private_subnets
 
