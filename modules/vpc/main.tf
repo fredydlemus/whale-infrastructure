b30eab0a30617387adff55eade30cd755eb9a5a9
fredyfloreslemus <fredyfloreslemus@gmail.com>
2025-03-24 11:20:32 -0600

    adding vpc module

---
+++ modules/vpc/main.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+    source = "terraform-aws-modules/vpc/aws"
+    version = "3.14.2"
+    name = var.vpc_name
+    cidr = var.vpc_cidr
+
+    azs = var.azs
+    public_subnets = var.public_subnets
+    private_subnets = var.private_subnets
+
+    enable_nat_gateway = true
+    single_nat_gateway = true
+
+    tags = var.tags
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,30 @@
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "A map of tags to apply to the vpc."
+  type        = map(string)
+  default     = {}
+}
